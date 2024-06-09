3a9f512076d52b367a5f39ea9d0a18281b4ba36a
kinnetik0 <alexis.rosquete@gmail.com>
2024-06-09 19:59:12 +0200

    Update -countdown done -stop button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,15 +3,48 @@ import PropTypes from "prop-types";
 
 //create your first component
 const Home = (props) => {
+    const stopCounter = () => {
+        clearInterval(props.intervalId); // Detener el contador
+    };
+
     return (
-        <div className="bigClock d-flex justify-content-center">
-            <div className="image">
-                <i className="fa-regular fa-clock"></i>
+        <div className="home">
+            <div className="bigClock d-flex justify-content-center">
+                <div className="image">
+                    <i className="fa-regular fa-clock"></i>
+                </div>
+                <div className="fourthDigit">{props.fourthDigit % 10}</div>
+                <div className="thirdDigit">{props.thirdDigit % 10}</div>
+                <div className="secondDigit">{props.secondDigit % 10}</div>
+                <div className="firstDigit">{props.firstDigit % 10}</div>
             </div>
-            <div className="fourthDigit">{props.fourthDigit % 10}</div>
-            <div className="thirdDigit">{props.thirdDigit % 10}</div>
-            <div className="secondDigit">{props.secondDigit % 10}</div>
-            <div className="firstDigit">{props.firstDigit % 10}</div>
+            <div className="input-group my-2 input-section d-flex">
+                <input
+                    type="number"
+                    min="0"
+                    max="9999"
+                    className="form-control"
+                    placeholder="Ingrese un número para cuenta atrás"
+                    aria-describedby="button-addon2"
+                />
+                <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                    onClick={props.onStartCountdown}
+                >
+                    Iniciar
+                </button>
+            </div>
+            <div className="d-flex justify-content-end">
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    onClick={stopCounter}
+                >
+                    Parar
+                </button>
+            </div>
         </div>
     );
 };
@@ -21,6 +54,8 @@ Home.prototypes = {
     thirdDigit: PropTypes.number,
     secondDigit: PropTypes.number,
     firstDigit: PropTypes.number,
+    onStartCountdown: PropTypes.func,
+    intervalId: PropTypes.number,
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,53 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-let counter = 0;
-setInterval(function () {
+const renderCounter = (counter) => {
     const fourthDigit = Math.floor(counter / 1000);
     const thirdDigit = Math.floor(counter / 100);
     const secondDigit = Math.floor(counter / 10);
     const firstDigit = Math.floor(counter / 1);
     counter++;
-    console.log(fourthDigit, thirdDigit, secondDigit, firstDigit);
     ReactDOM.render(
         <Home
             firstDigit={firstDigit}
             secondDigit={secondDigit}
             thirdDigit={thirdDigit}
             fourthDigit={fourthDigit}
+            onStartCountdown={startCountdown}
+            intervalId={intervalId}
         />,
         document.querySelector("#app")
     );
-}, 1000);
+};
+
+let counter = 0;
+let intervalId = null;
+
+// Función para iniciar la cuenta atrás
+const startCountdown = () => {
+    const input = document.querySelector("input[type='number']");
+    let initialCount = parseInt(input.value, 10);
+    if (isNaN(initialCount)) return;
+
+    // Clear the initial counter interval
+    clearInterval(intervalId);
+
+    // Start the countdown
+    intervalId = setInterval(() => {
+        initialCount--;
+        renderCounter(initialCount);
+        if (initialCount <= 0) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
+};
+
+// Start the initial counter on page load
+window.onload = () => {
+    intervalId = setInterval(() => {
+        counter++;
+        renderCounter(counter);
+    }, 1000);
+};
